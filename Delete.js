fd112531da03179c2e674f28d50d51eec66695d4
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-15 20:55:46 +0800

    Delete Delete.js

--- Delete.js
+++
@@ -1,8 +0,0 @@
-// Delete event in database and update event to agenda table
-function deleteEvent(id) {
-  // detlet event from database
-  let event = firebase.database().ref("Your Reference Database" + id);
-  event.remove();
-
-  readEvent();
-}
