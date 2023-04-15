45d256eae466b9cd7cced4a1342bd776326c38ee
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-15 21:27:31 +0800

    Update fireConfig.js

--- fireConfig.js
+++ fireConfig.js
@@ -1,13 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD8zoJ7hMfdNCslCRqCKJoFP7e6RSHzS0g",
+  authDomain: "actdb-294c8.firebaseapp.com",
+  projectId: "actdb-294c8",
+  storageBucket: "actdb-294c8.appspot.com",
+  messagingSenderId: "709588756544",
+  appId: "1:709588756544:web:62ce74e06ca0b0309c57a5"
+};
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyD8zoJ7hMfdNCslCRqCKJoFP7e6RSHzS0g",
-    authDomain: "actdb-294c8.firebaseapp.com",
-    projectId: "actdb-294c8",
-    storageBucket: "actdb-294c8.appspot.com",
-    messagingSenderId: "709588756544",
-    appId: "1:709588756544:web:62ce74e06ca0b0309c57a5"
-  };
-
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
+ // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
