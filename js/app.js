b756f7213e4614e03d4e105be57daadeae4951ce
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-16 13:42:25 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCKUfp6W42lIIjFFkT12Grf7_u2qAnMGS8",
+    authDomain: "zubcov-angular-crud.firebaseapp.com",
+    databaseURL: "https://zubcov-angular-crud.firebaseio.com",
+    projectId: "zubcov-angular-crud",
+    storageBucket: "zubcov-angular-crud.appspot.com",
+};
+firebase.initializeApp(config);
