39cff5d7edb78a78771e68a2e546bd1b3c6227a0
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-16 13:47:57 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var config = {
-    apiKey: "AIzaSyCKUfp6W42lIIjFFkT12Grf7_u2qAnMGS8",
-    authDomain: "zubcov-angular-crud.firebaseapp.com",
-    databaseURL: "https://zubcov-angular-crud.firebaseio.com",
-    projectId: "zubcov-angular-crud",
-    storageBucket: "zubcov-angular-crud.appspot.com",
+   apiKey: "AIzaSyDpnwboJUwHtx0-L2z1QYvfuxTQbSuWJRk",
+    authDomain: "crud-d7cb9.firebaseapp.com",
+    projectId: "crud-d7cb9",
+    storageBucket: "crud-d7cb9.appspot.com",
+    messagingSenderId: "80177244965",
+    appId: "1:80177244965:web:c8db16de1da90abb5b88cc"
 };
 firebase.initializeApp(config);
