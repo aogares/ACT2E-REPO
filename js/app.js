91d5b7fab17f45a30053045f7a2077c076bad932
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-16 14:22:34 +0800

    Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var config = {
-   apiKey: "AIzaSyDpnwboJUwHtx0-L2z1QYvfuxTQbSuWJRk",
-    authDomain: "crud-d7cb9.firebaseapp.com",
-    projectId: "crud-d7cb9",
-    storageBucket: "crud-d7cb9.appspot.com",
-    messagingSenderId: "80177244965",
-    appId: "1:80177244965:web:c8db16de1da90abb5b88cc"
-};
-firebase.initializeApp(config);
