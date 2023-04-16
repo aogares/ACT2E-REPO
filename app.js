7f68cb3e8b068310b77c118f78d6fbc87afd0128
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-16 15:41:03 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCSQO3WY8KmfVXM6siqZVj4-hqHbB4rirk",
+    authDomain: "fir-crud-dc0ee.firebaseapp.com",
+    databaseURL: "https://fir-crud-dc0ee-default-rtdb.firebaseio.com",
+    projectId: "fir-crud-dc0ee",
+    storageBucket: "fir-crud-dc0ee.appspot.com",
+    messagingSenderId: "578294013427",
+    appId: "1:578294013427:web:54894730874ac48b80c686"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
