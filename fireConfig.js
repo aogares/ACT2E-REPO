db2499950ad52f79984c83c7a102cd64a6ae23d4
Angela A. Ogares <125331582+aogares@users.noreply.github.com>
2023-04-20 16:17:58 +0800

    Create fireConfig.js

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCSQO3WY8KmfVXM6siqZVj4-hqHbB4rirk",
+    authDomain: "fir-crud-dc0ee.firebaseapp.com",
+    databaseURL: "https://fir-crud-dc0ee-default-rtdb.firebaseio.com",
+    projectId: "fir-crud-dc0ee",
+    storageBucket: "fir-crud-dc0ee.appspot.com",
+    messagingSenderId: "578294013427",
+    appId: "1:578294013427:web:54894730874ac48b80c686"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
